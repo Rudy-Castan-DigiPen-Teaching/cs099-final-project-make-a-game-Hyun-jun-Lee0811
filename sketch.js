cf3a954ccd72b8a7fb3f1a60c360110923799d45
Hyunjun Lee <Hyunjun.Lee@digipen.edu>
2021-06-06 03:33:31 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,13 +4,34 @@
 // Course     : CS099
 // Spring 2021
 
+var button_left = 250
+var button_bottom = 400
+var button_width  = 200
+var button_height = 70
+var button_label = 'START'
+var button_mouseIsOver = false
+var button_mouseWasPressed = false
+
 function setup()
 {
-    createCanvas( 800, 600 );
-
+    createCanvas( 800, 800 );
 }
+
+const title_size = 128
 
 function draw()
 {
     background( 220 );
+    const left = button_left
+    const right = button_left + button_width
+    const bottom = button_bottom
+    const top = button_bottom + button_height
+
+    const within_x = mouseX > left && mouseX < right
+    const within_y = mouseY > bottom && mouseY < top
+    button_mouseIsOver = 
+
+    fill(0)
+    translate(button_left, button_bottom)
+    rect(0, 0, button_width, button_height)
 }
